fe9d03d487a8ffb70329e9a1059d02b08867a3f7
Krishnna <kcsarrdah@gmail.com>
2021-04-03 16:51:59 +0530

    minors

--- src/pages/myPerks.jsx
+++ src/pages/myPerks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/header/header';
+import Header from '../components/header/commonHeader';
 
 
 
@@ -49,6 +49,41 @@ function MyPerks() {
                                 <p class="text-xs text-gray-500 mt-3">Terms and conditions</p>
                             </div>
                         </div>
+                        <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+                            <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+                                <h2 class="text-sm tracking-widest title-font mb-1 font-medium">date</h2>
+                                <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">Perk</h1>
+                                <p class="flex items-center text-gray-600 mb-2">
+                                    <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>description line
+                                </p>
+                                <p class="flex items-center text-gray-600 mb-2">
+                                    <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>description line
+                                </p>
+                                <p class="flex items-center text-gray-600 mb-6">
+                                    <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>description line
+                                </p>
+                                <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">redeem
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </button>
+                                <p class="text-xs text-gray-500 mt-3">Terms and conditions</p>
+                            </div>
+                        </div>
+
+
 
                         <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
                             <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
@@ -118,6 +153,7 @@ function MyPerks() {
                         </div>
                     </div>
                 </div>
+
                 <section class="text-gray-600 body-font">
                     <div class="container px-5 py-24 mx-auto flex items-center md:flex-row flex-col">
                         <div class="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
