8dc82826fb30406aacfce54a7ec57acc85041e36
Krishnna <kcsarrdah@gmail.com>
2021-04-06 15:50:49 +0530

    routes set and pages created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 import AdminProfile from "./pages/adminProfilePage";
 import EmpManagement from "./pages/EmpManagement";
 import LoginPage from "./pages/loginPage";
 import MyPerks from "./pages/myPerks";
 import MyTransactions from "./pages/myTransactions";
+import EmployeeDashboard from "./pages/employeeDashboard.jsx";
+
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-      <MyPerks />
-      <EmpManagement />
-      <MyTransactions />
-      {/* <AdminProfile /> */}
-    </div>
+    <Router>
+      <Switch>
+
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
+
+        <Route path="/myPerks" exact>
+          <MyPerks />
+        </Route>
+
+        <Route path="/empManagement" exact>
+          <EmpManagement />
+        </Route>
+
+        <Route path="/myTransaction" exact>
+          <MyTransactions />
+        </Route>
+
+        <Route path="/adminProfile" exact>
+          <AdminProfile />
+        </Route>
+
+        <Route path="/empDashboard" exact>
+          <EmployeeDashboard />
+        </Route>
+        <Route path="/" exact>
+
+        </Route>
+        <Route path="/" exact>
+
+        </Route>
+        <Route path="/" exact>
+
+        </Route>
+
+
+
+
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/employeeDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmployeeProfile = () => {
+    return(
+      <h1>Coming Soon...</h1>
+    );}
+
+export default EmployeeProfile;
+
